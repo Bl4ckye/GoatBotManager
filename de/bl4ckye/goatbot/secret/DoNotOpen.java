cd344972ab52aaa5d8bea7f3f72564720e34dbe5
Bl4ckye <120279299+Bl4ckye@users.noreply.github.com>
2023-03-05 10:53:52 +0100

    Update DoNotOpen.java

--- de/bl4ckye/goatbot/secret/DoNotOpen.java
+++ de/bl4ckye/goatbot/secret/DoNotOpen.java
@@ -2,6 +2,6 @@ package de.bl4ckye.goatbot.secret;
 
 public class DoNotOpen {
 
-    public static final String TOKEN = "MTA4MTY0MDA0MjI1NjE0NjU5NA.G1o89g.gcXGtC86Bo79vdrq1LxDgIsnR93a4yCsqe8VLg";
+    public static final String TOKEN = "******************************************************************";
 
 }
